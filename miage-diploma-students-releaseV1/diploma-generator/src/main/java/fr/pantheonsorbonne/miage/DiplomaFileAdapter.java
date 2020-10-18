19c9a9def8d85db19b023e14e09e990d8f8a32dd
Richard_nkr <61892549+Richard-Nkr@users.noreply.github.com>
2020-10-18 20:04:36 +0200

    merge

---
+++ miage-diploma-students-releaseV1/diploma-generator/src/main/java/fr/pantheonsorbonne/miage/DiplomaFileAdapter.java
@@ -0,0 +1,30 @@
+package fr.pantheonsorbonne.miage;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import com.google.common.io.ByteStreams;
+
+
+
+public class DiplomaFileAdapter extends FileGenerator<AbstractDiplomaGenerator> {
+
+	public DiplomaFileAdapter(AbstractDiplomaGenerator generator) {
+		super(generator);
+
+	}
+
+	@Override
+	public void generateFile(String outputFile) throws FailedGenerateException {
+		try (FileOutputStream fos = new FileOutputStream(outputFile)) {
+			InputStream is = this.generator.getContent();
+			ByteStreams.copy(is, fos);
+			is.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+			throw new FailedGenerateException("failed to write diploma file", e);
+		}
+	}
+
+}
